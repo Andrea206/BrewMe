a51efed7d9b415024d2afe97b55c8f18e5b83281
Andrea206 <drea567@gmail.com>
2019-05-14 23:19:56 -0700

    Adding JavaDoc comments

--- app/src/main/java/edu/uw/tacoma/group7/brewme/MySearchListRecyclerViewAdapter.java
+++ app/src/main/java/edu/uw/tacoma/group7/brewme/MySearchListRecyclerViewAdapter.java
@@ -17,11 +17,22 @@ public class MySearchListRecyclerViewAdapter extends RecyclerView.Adapter<MySear
     private final List<Brewery> mValues;
     private final OnListFragmentInteractionListener mListener;
 
+    /**
+     * Constructor for MySearchListRecyclerViewAdapter, requires a List<Brewery> object and OnListFragmentInteractionListener.
+     * @param items List<Brewery> object.
+     * @param listener OnListFragmentInteractionListener.
+     */
     public MySearchListRecyclerViewAdapter(List<Brewery> items, OnListFragmentInteractionListener listener) {
         mValues = items;
         mListener = listener;
     }
 
+    /**
+     * View holder method that calls parent view and returns created ViewHolder.
+     * @param parent ViewGroup.
+     * @param viewType integer id.
+     * @return ViewHolder.
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
@@ -29,6 +40,12 @@ public class MySearchListRecyclerViewAdapter extends RecyclerView.Adapter<MySear
         return new ViewHolder(view);
     }
 
+
+    /**
+     * Populates the Viewholder with formatted list data, onClickListener listens for item clicks.
+     * @param holder ViewHolder.
+     * @param position integer value.
+     */
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
@@ -48,11 +65,18 @@ public class MySearchListRecyclerViewAdapter extends RecyclerView.Adapter<MySear
         });
     }
 
+    /**
+     * Returns the number of items in brewery list object.
+     * @return integer.
+     */
     @Override
     public int getItemCount() {
         return mValues.size();
     }
 
+    /**
+     * ViewHolder class.
+     */
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final TextView mIdView;
--- app/src/main/java/edu/uw/tacoma/group7/brewme/SearchListFragment.java
+++ app/src/main/java/edu/uw/tacoma/group7/brewme/SearchListFragment.java
@@ -39,10 +39,7 @@ public class SearchListFragment extends Fragment {
     private String mSearchKey;
     private String mSearchValue;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
+
     public SearchListFragment() {
     }
 
@@ -64,6 +61,7 @@ public class SearchListFragment extends Fragment {
         }
     }
 
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
